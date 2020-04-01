318f103b94e65dba138903f04b8b3ebc199cc9e6
jonganebski <62702084+jonganebski@users.noreply.github.com>
2020-04-01 17:01:13 +0900

    Changed it to make proper sentence.

--- 2.py
+++ 2.py
@@ -1,10 +1,12 @@
 p = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj. "
 alphabet = "abcdefghijklmnopqrstuvwxyz"
+answer = ""
 for i in p:
   if i >= alphabet:
     nindex = alphabet.index(i) + 2
     if nindex > 25:
       nindex -= 26
-    print(alphabet[nindex])
+    answer += alphabet[nindex]
   else:
-    print(i)
+    answer += i
+print(answer)
