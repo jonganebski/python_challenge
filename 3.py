d8da2193ce2d710972410ff060185b4f84df9346
jonganebski <62702084+jonganebski@users.noreply.github.com>
2020-04-02 12:45:14 +0900

    trying to solve...

--- 3.py
+++ 3.py
@@ -1,8 +1,18 @@
 # http://www.pythonchallenge.com/pc/def/equality.html
 code = "abCDEfGHIjk"
+list = []
 for i in code:
   if i.isupper() is True:
-    print("True")
+    list.append(int(True))
   else:
-    print("False")
-    
+    list.append(int(False))
+sum = 0
+for n in list:
+  if n = 1: 
+    sum += 1
+    if sum == 3:
+      
+  else:
+    sum = 0
+
+  
