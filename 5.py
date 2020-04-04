28212473eeb015b55b8480bf842cd8cadf395ff1
jonganebski <62702084+jonganebski@users.noreply.github.com>
2020-04-04 18:27:16 +0900

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,3 +1,11 @@
 # http://www.pythonchallenge.com/pc/def/peak.html
 
+import urllib.request
 import pickle
+
+read_file = urllib.request.urlopen('http://www.pythonchallenge.com/pc/def/banner.p').read()
+
+open_file = pickle.loads(read_file)
+
+for i in open_file:
+    print(i)
