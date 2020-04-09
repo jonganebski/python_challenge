2bdb8bdf5b20475baa91a46795d7d1e4713c097b
jonganebski <62702084+jonganebski@users.noreply.github.com>
2020-04-09 18:17:26 +0900

    Update 7.py

--- 7.py
+++ 7.py
@@ -1,19 +1,24 @@
-# http://www.pythonchallenge.com/pc/def/oxygen.html
-
 from PIL import Image
+
 image = Image.open('oxygen.png')
 print(image.size)
 # im = image.load()
 im_rgb = image.convert('RGB')
 (width, height) = image.size
 
-for h in range(height):
-    same_rgb = []
-    for w in range(width):
-        rgb = im_rgb.getpixel((w, h))
-        if rgb[0] == rgb[1] == rgb[2]:
-            same_rgb.append(rgb)
-    print(same_rgb)
-
+# for h in range(height):
+#     same_rgb = []
+#     for w in range(width):
+#         rgb = im_rgb.getpixel((w, h))
+#         if rgb[0] == rgb[1] == rgb[2]:
+#             same_rgb.append(rgb)
+#             print(w, h)
+# (0, 43) ~ (607, 51)
 # for i in range(0, width):
 
+for i in range(width):
+    rgb = im_rgb.getpixel((i, 43))
+    if rgb[0] == rgb[1] == rgb[2]:
+        print(chr(rgb[0]))
+    else:
+        print(rgb)
