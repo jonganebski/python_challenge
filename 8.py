8c04258fffc296f585db950c315117ede6793091
jonganebski <62702084+jonganebski@users.noreply.github.com>
2020-04-10 22:20:38 +0900

    Update 8.py
    Nope.

--- 8.py
+++ 8.py
@@ -1,1 +1,18 @@
 # http://www.pythonchallenge.com/pc/def/integrity.html
+
+from bs4 import BeautifulSoup
+import requests
+import re
+URL = 'http://www.pythonchallenge.com/pc/def/integrity.html'
+
+request = requests.get(URL)
+html = BeautifulSoup(request.text, 'html.parser')
+# print(html)
+area_cords = html.find_all('area')
+area_cords = str(area_cords)
+cords_nums = re.findall('\d+', area_cords)
+print(cords_nums)
+pw = ''
+for n in cords_nums:
+    pw += (chr(int(n)))
+print(pw)
